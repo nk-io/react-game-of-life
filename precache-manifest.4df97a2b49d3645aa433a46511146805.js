bf30375d27497f883a955e3cdb9ad3249db01ffd
nk-io <heysemkorkmaz@gmail.com>
2020-08-20 19:05:51 +0200

    Updates

---
+++ precache-manifest.4df97a2b49d3645aa433a46511146805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185c28d2034460c7b938fa586149b96b",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "18b29f8b17caa1832c87",
+    "url": "/react-game-of-life/static/css/main.d3bcd381.chunk.css"
+  },
+  {
+    "revision": "a36e5648bf0e4c2ffd5f",
+    "url": "/react-game-of-life/static/js/2.6372262b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-game-of-life/static/js/2.6372262b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b29f8b17caa1832c87",
+    "url": "/react-game-of-life/static/js/main.4a134c0d.chunk.js"
+  },
+  {
+    "revision": "12c9f28f8797154225dc",
+    "url": "/react-game-of-life/static/js/runtime-main.906f54b3.js"
+  }
+]);
